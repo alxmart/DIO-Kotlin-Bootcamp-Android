dccd3a1f63b96c6e7a1e779fcd09824ad2b14d79
alxmart <alxmartinez@gmail.com>
2022-11-05 14:36:13 -0300

    Adding more exercises and examples.

--- src/main/kotlin/Functions-DefaultParameterNamedArgs.kt
+++ src/main/kotlin/Functions-DefaultParameterNamedArgs.kt
@@ -14,9 +14,14 @@ fun multiply(x: Int, y: Int) = x * y                                    // 4
 
 fun main() {
     printMessage("Hello")                                               // 5
+
     printMessageWithPrefix("Hello", "Log")                              // 6
+
     printMessageWithPrefix("Hello")                                     // 7
+
     printMessageWithPrefix(prefix = "Log", message = "Hello")           // 8
+
     println(sum(1, 2))                                                  // 9
+
     println(multiply(2, 4))                                             // 10
 }
