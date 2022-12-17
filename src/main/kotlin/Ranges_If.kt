39e1662c640f5549da636d598ecec93559332891
alxmart <alxmartinez@gmail.com>
2022-12-17 20:24:02 -0300

    Adding more exercises and examples.

--- src/main/kotlin/Ranges_If.kt
+++ src/main/kotlin/Ranges_If.kt
@@ -3,12 +3,12 @@ fun main() {
     val x = 2
 
     if (x in 1..5) {            // 1
-        print("x is in range from 1 to 5")
+        print("x is $x.  x is in range from 1 to 5")
     }
     println()
 
 
     if (x !in 6..10) {          // 2
-        print("x is not in range from 6 to 10")
+        print("x is $x .  x is not in range from 6 to 10")
     }
 }
