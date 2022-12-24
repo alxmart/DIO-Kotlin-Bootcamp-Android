00cac3bdd3d91a34b9d3c6f0a3a8abbe4e6b735b
alxmart <alxmartinez@gmail.com>
2022-12-24 12:18:37 -0300

    Adding more exercises and examples.

---
+++ src/main/kotlin/Sealed_CLasses.kt
@@ -0,0 +1,21 @@
+sealed class Mammal(val name: String)                                                   // 1
+
+class Cat(val catName: String) : Mammal(catName)                                        // 2
+
+class Human(val humanName: String, val job: String) : Mammal(humanName)
+
+fun greetMammal(mammal: Mammal): String {
+    when (mammal) {                                                                     // 3
+        is Human -> return "Hello ${mammal.name}; You're working as a ${mammal.job}"    // 4
+        is Cat -> return "Hello ${mammal.name}"                                         // 5
+    }                                                                                   // 6
+}
+
+fun main() {
+    val cat = Cat("Fefe")
+    val human = Human("Homer","Nuclear Security Analyst")
+    //println(greetMammal(Cat("Snowy")))
+
+    println(greetMammal(cat))
+    println(greetMammal(human))
+}
