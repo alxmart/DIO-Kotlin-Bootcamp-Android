97ab37ee7b8cb82dd33633c5c4dd4a581f65f783
alxmart <alxmartinez@gmail.com>
2022-12-25 11:36:36 -0300

    Adding more exercises and examples.

--- src/main/kotlin/Scope_Functions_With.kt
+++ src/main/kotlin/Scope_Functions_With.kt
@@ -1,13 +1,17 @@
 
-class Configuration(var host: String, var port: Int)
+class Configuration(val host: String, val port: Int)
 
 fun main() {
     val configuration = Configuration(host = "127.0.0.1", port = 9000)
 
-
     with(configuration) {
         println("$host:$port")
     }
+    // Alternativa:
+    configuration.run {
+        println("$host:$port")
+    }
+
 
     // instead of:
     println("${configuration.host}:${configuration.port}")
