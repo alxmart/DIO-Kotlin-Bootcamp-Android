4144c6177468939faa0b9d56e4f241762d71a826
alxmart <alxmartinez@gmail.com>
2023-08-20 15:11:00 -0300

    Adding more exercises and examples.

--- src/main/kotlin/ThrowException.kt
+++ src/main/kotlin/ThrowException.kt
@@ -1,3 +1,20 @@
+import kotlin.jvm.Throws
+
+class IllegalVoterException(message: String) : Throwable(message)
+
+@Throws(IllegalVoterException::class)
+fun vote(nome: String, age: Int) {
+    if (age < 16) {
+        throw IllegalVoterException("Apenas pessoas c/ 16 anos ou + podem votar")
+    }
+    println("Voto do $nome  realizado com sucesso")
+}
+
 fun main() {
-    throw Exception("Olá ")
+
+    vote("Homer", 48)
+    vote("Lisa", 10)
+
+
+
 }
